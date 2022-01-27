fbad6f2158b603567d5ccbb4346a55e3717be4c6
William <binitiewilliam0511@gmail.com>
2022-01-27 21:40:46 +0100

    first commit

---
+++ styles.js
@@ -0,0 +1,31 @@
+import { Platform, StyleSheet, StatusBar } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+
+  box: {
+    width: 300,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderRadius: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategrey",
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
+  },
+});
